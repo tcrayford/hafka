e065439352db074d58afad2bf39098d5395636df
Tom Crayford <tcrayford@googlemail.com>
2012-06-26 11:37:16 +0100

    update benchmark to evaluate args before benching

--- src/Bench/Main.hs
+++ src/Bench/Main.hs
@@ -2,15 +2,36 @@
 import Criterion.Main
 import qualified Data.ByteString.Char8 as B
 import Kafka.Producer
+import Kafka.Consumer
 import Kafka.Types
+import Control.Exception(evaluate)
+import Control.Monad.Trans(liftIO)
+import Criterion.Config(defaultConfig)
+import Control.DeepSeq
 
-main = defaultMain [
-    bgroup "produce" [
-      bench "10p" $ whnf (fullProduceRequest producerSettings) (Message $ rawMessage 10)
-      , bench "100p" $ whnf (fullProduceRequest producerSettings) (Message $ rawMessage 100)
+instance NFData B.ByteString
+
+main = do
+  let p10 = rawMessage 10
+      p100 = rawMessage 100
+      c10 = rawMessageSet 10
+      c100 = rawMessageSet 100
+      producerSettings = ProducerSettings (Topic "test") (Partition 0)
+      consumerSettings = Consumer (Topic "test") (Partition 0) (Offset 0)
+      eval = (liftIO . evaluate $ rnf [p10, p100, c10, c100])
+
+  defaultMainWith defaultConfig eval [
+      bgroup "produce" [
+        bench "10p" $ whnf (fullProduceRequest producerSettings) $ Message p10
+        , bench "100p" $ whnf (fullProduceRequest producerSettings) $ Message p100
+      ],
+      bgroup "consume" [
+        bench "10c" $ whnf (parseMessageSet c10) consumerSettings
+        , bench "100c" $ whnf (parseMessageSet c100) consumerSettings
+      ]
     ]
-  ]
-  where
-    producerSettings = ProducerSettings (Topic "test") (Partition 0)
 
-rawMessage n = B.pack . take n $ repeat 'a'
+rawMessage n = id $! B.pack . take n $ repeat 'a'
+
+rawMessageSet n = id $! putMessage message
+  where message = Message $! B.pack . take n $ repeat 'a'
