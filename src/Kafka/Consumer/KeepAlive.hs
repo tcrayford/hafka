4bc1d475fb19d09b5237ac404169438a315ee11d
Tom Crayford <tcrayford@googlemail.com>
2012-06-27 10:27:02 +0100

    add keep alive consumer

--- src/Kafka/Consumer/KeepAlive.hs
+++ src/Kafka/Consumer/KeepAlive.hs
@@ -1,9 +1,15 @@
 module Kafka.Consumer.KeepAlive where
 import Kafka.Consumer
+import Kafka.Parsing
+import Kafka.Response
+import Kafka.Types
 import Kafka.Network
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString
 import System.IO
+import qualified Data.ByteString.Char8 as B
+import Data.ByteString.Char8(ByteString)
+import Data.Serialize.Get
 
 data KeepAliveConsumer = KeepAliveConsumer {
     kaConsumer :: BasicConsumer
@@ -13,7 +19,8 @@ data KeepAliveConsumer = KeepAliveConsumer {
 instance Consumer KeepAliveConsumer where
   consume c = do
     let s = kaSocket c
-    result <- readDataResponse h
+    send s $ consumeRequest c
+    result <- readDataResponse' s
     case result of
       (Right r) -> return $! parseMessageSet r c
       (Left r) -> do 
@@ -24,6 +31,16 @@ instance Consumer KeepAliveConsumer where
   getStream c = getStream $ kaConsumer c
   increaseOffsetBy c n = c { kaConsumer = newC }
     where newC = increaseOffsetBy (kaConsumer c) n
+
+readDataResponse' :: Socket -> IO (Either ErrorCode ByteString)
+readDataResponse' s = do
+  d <- recv s 4096
+  let (Right dataLength) = runGet getDataLength d
+      rawResponse = B.take dataLength (B.drop 4 d)
+      x = parseErrorCode rawResponse
+  case x of
+    Success -> return $! Right $ B.drop 2 rawResponse
+    e -> return $! Left e
 
 keepAlive :: BasicConsumer -> IO KeepAliveConsumer
 keepAlive c = do
