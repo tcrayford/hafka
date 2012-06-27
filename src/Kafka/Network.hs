aba1cb3270e9efba2c67f0af028d2de96b7a4164
Tom Crayford <tcrayford@googlemail.com>
2012-06-27 10:06:40 +0100

    add networking module

---
+++ src/Kafka/Network.hs
@@ -0,0 +1,42 @@
+module Kafka.Network(
+  N.PortID(..),
+  connectTo
+  ) where
+import Network.Socket
+import Network.BSD
+import qualified Network as N
+import System.IO
+import qualified Control.Exception as Exception
+
+connectTo :: HostName -> N.PortID -> IO Socket
+connectTo hostname (N.PortNumber port) = connect' hostname (show port)
+
+connect' :: HostName -> ServiceName -> IO Socket
+connect' host serv = do
+    proto <- getProtocolNumber "tcp"
+    let hints = defaultHints { addrFlags = [AI_ADDRCONFIG]
+                             , addrProtocol = proto
+                             , addrSocketType = Stream }
+    addrs <- getAddrInfo (Just hints) (Just host) (Just serv)
+    firstSuccessful $ map tryToConnect addrs
+  where
+  tryToConnect addr =
+    Exception.bracketOnError
+        (socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr))
+        (sClose)  -- only done if there's an error
+        (\sock -> do
+          connect sock (addrAddress addr)
+          return sock
+        )
+
+firstSuccessful :: [IO a] -> IO a
+firstSuccessful [] = error "firstSuccessful: empty list"
+firstSuccessful (p:ps) = catchIO p $ \e ->
+    case ps of
+        [] -> Exception.throw e
+        _  -> firstSuccessful ps
+
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+catchIO = Exception.catch
+
+--socketToHandle sock ReadWriteMode
