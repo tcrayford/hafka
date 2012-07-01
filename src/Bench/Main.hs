d1b7f14c8748c35b62296385b25a87ecd2a6890b
Tom Crayford <tcrayford@googlemail.com>
2012-07-01 11:58:03 +0100

    add end to end benchmarks

--- src/Bench/Main.hs
+++ src/Bench/Main.hs
@@ -4,11 +4,14 @@ import qualified Data.ByteString.Char8 as B
 import Kafka.Producer
 import Kafka.Consumer
 import Kafka.Consumer.Basic
+import Kafka.Consumer.KeepAlive
 import Kafka.Types
 import Control.Exception(evaluate)
 import Control.Monad.Trans(liftIO)
 import Criterion.Config(defaultConfig)
 import Control.DeepSeq
+import Control.Concurrent.MVar
+import Specs.IntegrationHelper
 
 instance NFData B.ByteString
 
@@ -23,12 +26,17 @@ main = do
 
   defaultMainWith defaultConfig eval [
       bgroup "produce" [
-        bench "10p" $ whnf (fullProduceRequest producerSettings) $ [Message p10]
+          bench "10p" $ whnf (fullProduceRequest producerSettings) $ [Message p10]
         , bench "100p" $ whnf (fullProduceRequest producerSettings) $ [Message p100]
       ],
       bgroup "consume" [
-        bench "10c" $ whnf (parseMessageSet c10) consumerSettings
+          bench "10c" $ whnf (parseMessageSet c10) consumerSettings
         , bench "100c" $ whnf (parseMessageSet c100) consumerSettings
+      ],
+
+      bgroup "roundtrip" [
+          bench "roundtripBasicConsumer" $ roundtripBasicConsumer
+        , bench "roundtripKeepAliveConsumer" $ roundtripKeepAliveConsumer
       ]
     ]
 
@@ -36,3 +44,29 @@ rawMessage n = B.pack . take n $ repeat 'a'
 
 rawMessageSet n = putMessage message
   where message = Message $! B.pack . take n $ repeat 'a'
+
+roundtripBasicConsumer :: IO ()
+roundtripBasicConsumer = do
+  let stream = Stream (Topic "bench_basic_consumer_roundtrip") (Partition 0)
+      (testProducer, testConsumer) = coupledProducerConsumer stream
+      message = Message "bench basic consumer roundtrip"
+
+  result <- newEmptyMVar
+  produce testProducer [message]
+  recordMatching testConsumer message result
+
+  waitFor result message (return ())
+
+roundtripKeepAliveConsumer :: IO ()
+roundtripKeepAliveConsumer = do
+  let stream = Stream (Topic "bench_keep_alive_consumer_roundtrip") (Partition 0)
+      (testProducer, testConsumer) = coupledProducerConsumer stream
+      message = Message "bench keep alive consumer roundtrip"
+
+  c <- keepAlive testConsumer
+  result <- newEmptyMVar
+  produce testProducer [message]
+  recordMatching c message result
+
+  waitFor result message (return ())
+
