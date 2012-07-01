f05efc7b0900979947c9da3dccb0aeec462a5228
Tom Crayford <tcrayford@googlemail.com>
2012-07-01 02:45:24 +0100

    fix benchmarks

--- src/Bench/Main.hs
+++ src/Bench/Main.hs
@@ -3,6 +3,7 @@ import Criterion.Main
 import qualified Data.ByteString.Char8 as B
 import Kafka.Producer
 import Kafka.Consumer
+import Kafka.Consumer.Basic
 import Kafka.Types
 import Control.Exception(evaluate)
 import Control.Monad.Trans(liftIO)
@@ -16,14 +17,14 @@ main = do
       p100 = rawMessage 100
       c10 = rawMessageSet 10
       c100 = rawMessageSet 100
-      producerSettings = ProducerSettings (Topic "test") (Partition 0)
-      consumerSettings = Consumer (Topic "test") (Partition 0) (Offset 0)
+      producerSettings = ProducerSettings (Stream (Topic "test") (Partition 0))
+      consumerSettings = BasicConsumer (Stream (Topic "test") (Partition 0)) (Offset 0)
       eval = liftIO . evaluate $ rnf [p10, p100, c10, c100]
 
   defaultMainWith defaultConfig eval [
       bgroup "produce" [
-        bench "10p" $ whnf (fullProduceRequest producerSettings) $ Message p10
-        , bench "100p" $ whnf (fullProduceRequest producerSettings) $ Message p100
+        bench "10p" $ whnf (fullProduceRequest producerSettings) $ [Message p10]
+        , bench "100p" $ whnf (fullProduceRequest producerSettings) $ [Message p100]
       ],
       bgroup "consume" [
         bench "10c" $ whnf (parseMessageSet c10) consumerSettings
